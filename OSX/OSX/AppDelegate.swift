ed945135f4ff6c5f59071f6fdc81fc6502dafc8c
wanggx <gxwang@gxwangdeMacBook-Pro.local>
2015-07-13 13:47:21 +0800

    添加一个工程

---
+++ OSX/OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSX
+//
+//  Created by gxwang on 15/7/11.
+//  Copyright (c) 2015年 gxwang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
