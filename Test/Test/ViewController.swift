04ddbbc2f553d15ccca2ea71df4b85ee08dab47c
wanggx <wgxdyx.c2c@gmail.com>
2015-08-24 21:47:31 +0800

    添加CollectionView自动布局

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by gxwang on 15/8/24.
+//  Copyright (c) 2015年 gxwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 2
+        }
+        return 5
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as! UICollectionViewCell
+        
+        cell.backgroundColor = UIColor.brownColor()
+        return cell
+        
+    }
+
+}
+
