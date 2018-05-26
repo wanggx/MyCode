4f4b670c7a4745ad02c2d0a39eae6670e4585e6d
wanggx <wgxdyx.c2c@gmail.com>
2018-05-26 20:25:49 +0800

    初始化SpringBoot工程

---
+++ SpringBoot/SpringBoot/src/main/java/wgx/SpringBoot/App.java
@@ -0,0 +1,13 @@
+package wgx.SpringBoot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
