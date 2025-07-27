b81675b922bb35c677cd286a0b15f1a9770927fb
wanggx <wangguoxing@kingsoft.com>
2025-07-27 18:41:49 +0800

    async select

--- frontend/src/views/StockSelect.vue
+++ frontend/src/views/StockSelect.vue
@@ -108,7 +108,7 @@ const fetchData = async () => {
     if (res.data && res.data.data) {
       tableData.value = res.data.data.items
       total.value = res.data.data.total
-    } else if (res.data && res.data.error && res.data.error.includes('正在选股中')) {
+    } else if (res.data && res.data.error) {
       ElMessage.info(res.data.error)
       tableData.value = []
       total.value = 0
