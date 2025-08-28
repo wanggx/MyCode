c0b4dfa24d2f337f3cbd18c2ae2324eb262d9cae
wangguoxing <wangguoxing@kingsoft.com>
2025-08-28 19:43:45 +0800

    remove unused code

--- PycharmProjects/PythonProject/talibtest.py
+++ PycharmProjects/PythonProject/talibtest.py
@@ -14,3 +14,52 @@ table = pd.DataFrame({
 '该类别指标个数': list(map(lambda x: len(x), groups.values()))
 })
 print(table)
+
+# 从字典创建 DataFrame
+# data = {
+#     'name': ['Alice', 'Bob', 'Charlie'],
+#     'age': [25, 30, 35],
+#     'city': ['New York', 'London', 'Tokyo']
+# }
+# df = pd.DataFrame(data)
+# df.reset_index()
+# print(df.filter(items=[1,2], axis=0))
+
+
+# import pandas as pd
+# import numpy as np
+#
+# df = pd.DataFrame(np.random.randint(0,2,20), columns=['行为']).reset_index()
+# df['转换标记'] = df['行为']!=df['行为'].shift()  # 识别信号，判断行为是否发生了改变
+# df['行为分组'] = df['转换标记'].cumsum()  # 辅助列，根据识别信号，对相邻的相同行为进行分组，便于计算每组相同行为的连续发生次数
+# df['连续行为次数'] = df.groupby(['行为分组'])['index'].rank(method='dense')  # 根据行为分组，使用窗口函数对每条行为标记连续发生次数
+# print(df)
+
+
+
+# # 创建数据
+# data = {
+#     'date': pd.date_range(start='2021-01-01', periods=10, freq='D'),
+#     'value': [10, 15, 20, 20, 25, 30, 30, 35, 40, 45]
+# }
+# df = pd.DataFrame(data)
+# df.set_index('date', inplace=True)
+#
+# # 分析连续增长的天数
+# df['is_increasing'] = df['value'] > df['value'].shift(1)
+# df['consecutive_days'] = df['is_increasing'].cumsum() * df['is_increasing']
+# print(df)
+# # 筛选出连续增长超过3天的记录并重置索引
+# consecutive_days_filter = df.groupby('consecutive_days')['value'].filter(lambda x: len(x) > 2).reset_index(drop=True)
+# print(consecutive_days_filter)
+#
+# import pandas as pd
+df = pd.DataFrame({'date': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-05', '2023-01-06', '2023-01-07']})
+df['date'] = pd.to_datetime(df['date'])
+df['consecutive_days'] = df['date'].diff().cumsum()
+print(df)
+
+
+
+
+
--- stock/backend/data/select_ma.py
+++ stock/backend/data/select_ma.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 from moduledir.chatutil import sendMsg, sendGroupFile
 from moduledir.stockutil import getStockData, getStockList, getStockWeekData
+from pandas.conftest import axis_1
+
 
 def ma(df):
     if df is None or len(df) == 0:
@@ -71,7 +73,7 @@ def selectMaByDaily(date_str, n):
     stock_daily_df = getStockData(None, start_str, date_str)
 
     close_ma_df = stock_daily_df[['ts_code', 'trade_date', 'high', 'open', 'close', 'low']].groupby(['ts_code']).tail(60)
-    filter_df = close_ma_df.groupby(['ts_code']).filter(lambda x: x['trade_date'].max() == date_str)
+    filter_df = close_ma_df.groupby(['ts_code']).filter(lambda x: x['trade_date'].max() == date_str, axis = 0)
     stock_ma_df = processMa(filter_df)
     if stock_ma_df is None or len(stock_ma_df) == 0:
         sendMsg(f"{date_str}没有选中任何股票")
