b4472463f2247d8ba0f8b33c4c343b92abf4458f
wangguoxing <wangguoxing@kingsoft.com>
2025-11-03 19:37:55 +0800

    remove code

--- PycharmProjects/PythonProject/moduledir/Man.py
+++
@@ -1,8 +0,0 @@
-from moduledir.Person import Person
-
-
-class Man(Person):
-
-    def __init__(self, first_name, last_name, age):
-        super().__init__(first_name, age)
-        self.last_name = last_name
